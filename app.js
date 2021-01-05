f41423402aa8147527077547f7cef038938bf78a
Steven Woodward <steveanthony999@gmail.com>
2021-01-05 08:50:43 -0800

    added smooth scrolling

--- app.js
+++ app.js
@@ -54,3 +54,96 @@ navIcon.addEventListener('click', () => {
     navLinksMobile.style.transform = 'scaleY(0)';
   }
 });
+
+// Smooth Scrolling
+// Smooth Scrolling
+// Smooth Scrolling
+
+(function initSmoothScrolling() {
+  console.log('smooth scrolling');
+
+  const duration = 400;
+
+  const pageUrl = location.hash ? stripHash(location.href) : location.href;
+
+  (function delegatedLinkHijacking() {
+    document.body.addEventListener('click', onClick, false);
+
+    function onClick(e) {
+      if (!isInPageLink(e.target)) return;
+
+      e.stopPropagation();
+      e.preventDefault();
+
+      jump(e.target.hash, {
+        duration: duration,
+        callback: function () {
+          setFocus(e.target.hash);
+        },
+      });
+    }
+  })();
+
+  function isInPageLink(n) {
+    return n.tagName.toLowerCase() === 'a' && n.hash.length > 0 && stripHash(n.href) === pageUrl;
+  }
+
+  function stripHash(url) {
+    return url.slice(0, url.lastIndexOf('#'));
+  }
+
+  function setFocus(hash) {
+    var element = document.getElementById(hash.substring(1));
+
+    if (element) {
+      if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+        element.tabIndex = -1;
+      }
+
+      element.focus();
+    }
+  }
+
+  function jump(target, options) {
+    var start = window.pageYOffset,
+      opt = {
+        duration: options.duration,
+        offset: options.offset || 0,
+        callback: options.callback,
+        easing: options.easing || easeInOutQuad,
+      },
+      distance =
+        typeof target === 'string' ? opt.offset + document.querySelector(target).getBoundingClientRect().top : target,
+      duration = typeof opt.duration === 'function' ? opt.duration(distance) : opt.duration,
+      timeStart,
+      timeElapsed;
+
+    requestAnimationFrame(function (time) {
+      timeStart = time;
+      loop(time);
+    });
+
+    function loop(time) {
+      timeElapsed = time - timeStart;
+
+      window.scrollTo(0, opt.easing(timeElapsed, start, distance, duration));
+
+      if (timeElapsed < duration) requestAnimationFrame(loop);
+      else end();
+    }
+
+    function end() {
+      window.scrollTo(0, start + distance);
+
+      if (typeof opt.callback === 'function') opt.callback();
+    }
+
+    // Robert Penner's easeInOutQuad - http://robertpenner.com/easing/
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+  }
+})();
